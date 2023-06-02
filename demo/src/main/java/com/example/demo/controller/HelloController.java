46649dfb97422dad6b309e3872a845e54d5bf02c
Yuzunoha <studysteadystandby@gmail.com>
2023-06-02 16:04:25 +0900

    これはエラーが出ない

--- demo/src/main/java/com/example/demo/controller/HelloController.java
+++ demo/src/main/java/com/example/demo/controller/HelloController.java
@@ -1,13 +1,34 @@
 package com.example.demo.controller;
 
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.ListObjectsV2Result;
+import com.amazonaws.services.s3.model.S3ObjectSummary;
+
+import lombok.RequiredArgsConstructor;
+
 @RestController
+@RequiredArgsConstructor
 public class HelloController {
+
+  private final AmazonS3 s3Client;
 
   @RequestMapping("/")
   public String index() {
+    s3test2();
     return "indexです";
   }
+
+  private void s3test2() {
+    ListObjectsV2Result result = s3Client.listObjectsV2("test-bucket");
+    List<S3ObjectSummary> objects = result.getObjectSummaries();
+    for (S3ObjectSummary os : objects) {
+      System.out.println("* " + os.getKey());
+    }
+  }
+
 }
--- demo/src/main/java/com/example/demo/controller/S3Config.java
+++ demo/src/main/java/com/example/demo/controller/S3Config.java
@@ -3,6 +3,7 @@ package com.example.demo.controller;
 import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.client.builder.AwsClientBuilder.EndpointConfiguration;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import org.springframework.context.annotation.Bean;
@@ -14,14 +15,15 @@ public class S3Config {
   private String s3ClientRegion = "ap-northeast-1";
   private String accessKey = "dummy";
   private String secretKey = "dummy";
+  private String endpoint = "http://localhost:4566";
 
   @Bean
   public AmazonS3 s3Client() {
     AWSCredentials credentials = new BasicAWSCredentials(accessKey, secretKey);
     return AmazonS3ClientBuilder
-      .standard()
-      .withCredentials(new AWSStaticCredentialsProvider(credentials))
-      .withRegion(s3ClientRegion)
-      .build();
+        .standard()
+        .withEndpointConfiguration(new EndpointConfiguration(endpoint, s3ClientRegion))
+        .withCredentials(new AWSStaticCredentialsProvider(credentials))
+        .build();
   }
 }
